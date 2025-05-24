278100b2663eb018a3819ca08ac0a6ca1c72ecd7
Raj06z <yadu3791@gmail.com>
2025-05-24 14:26:55 +0530

    Add files via upload

---
+++ characterpicturegrid.py
@@ -0,0 +1,17 @@
+def print_grid(grid):
+    """Prints the grid rotated 90 degrees."""
+    for col in range(len(grid[0])):
+        for row in range(len(grid)):
+            print(grid[row][col], end=" ")
+        print()
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print_grid(grid)
